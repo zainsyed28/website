d10124760f98b707f8309b43d8d67716ed20fc3c
Zain Syed <zainsyed664@gmail.com>
2023-10-06 21:49:02 -0700

    added resume

--- src/components/About.js
+++ src/components/About.js
@@ -31,6 +31,10 @@ export default function About() {
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               Github
             </a>
+            <a href="Resume_Zain_Syed.pdf"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Resume
+            </a>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
